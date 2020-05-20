358aa92795c5464fbf3cb9db43f3d3702f59a603
Bar Horing Amir <bar.apple@gmail.com>
2020-05-20 08:53:45 +0300

    done useEffect Solution

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,24 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+
+/* Read and Write value of the counter from and to local storage */
+const getStateFromLocalStorage = () => {
+  const storage = localStorage.getItem('counterState');
+  if (storage) return JSON.parse(storage).count;
+  return { count: 0 };
+};
+
+const storeStateInLocalStorage = (count) => {
+  localStorage.setItem('counterState', JSON.stringify({ count }));
+  console.log(localStorage);
+};
 
 const Counter = ({ max, step }) => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(getStateFromLocalStorage());
+
+  /* It's ok to combine these two; A tradeof between Performance vs Architecture & Testabilty */
+  useEffect(() => {
+    storeStateInLocalStorage(count);
+  }, [count]);
 
   useEffect(() => {
     document.title = `Counter ${count}`;
